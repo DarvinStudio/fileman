040dde504482282b738343538656646d9f6940e2
igor <igor.sv.n@gmail.com>
2018-01-09 12:05:43 +0300

    Implement dummy push command.

---
+++ src/Darvin/Filer/Command/PushCommand.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2018, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Filer\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Push command
+ */
+class PushCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        if (null === $this->getName()) {
+            $this->setName('push');
+        }
+
+        $this->setDescription('Pushes local files to remote directory');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->success('Hello, world!');
+    }
+}
