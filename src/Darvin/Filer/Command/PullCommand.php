1001990a4d827186f3877d154ca82cb3a95ffae1
igor <igor.sv.n@gmail.com>
2018-01-09 11:54:15 +0300

    Implement dummy pull command.

---
+++ src/Darvin/Filer/Command/PullCommand.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2017, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Filer\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Pull command
+ */
+class PullCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        if (null === $this->getName()) {
+            $this->setName('pull');
+        }
+
+        $this->setDescription('Pulls remote files to local directory');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->success('Hello, world!');
+    }
+}
