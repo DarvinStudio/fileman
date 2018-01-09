ac18404662d056e02320a8f8c7430b422facdd96
igor <igor.sv.n@gmail.com>
2018-01-09 16:03:00 +0300

    Add "parameters" input option to commands.

--- src/Darvin/Filer/Command/AbstractCommand.php
+++ src/Darvin/Filer/Command/AbstractCommand.php
@@ -43,6 +43,7 @@ DESCRIPTION
             new InputOption('key', 'k', InputOption::VALUE_OPTIONAL, 'SSH private RSA key pathname relative to home directory', '.ssh/id_rsa'),
             new InputOption('password', 'p', InputOption::VALUE_NONE, 'Ask for SSH or SSH key password'),
             new InputOption('port', 'P', InputOption::VALUE_OPTIONAL, 'SSH server port', 22),
+            new InputOption('parameters', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Directory parameters', ['image_upload_path', 'upload_path']),
         ]);
     }
 
