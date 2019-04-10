940055874d2b094e082aabfa6ad7fc0fc17c8396
Igor Nikolaev <igor.sv.n@gmail.com>
2019-04-10 09:01:19 +0300

    Change date format in archive name.

--- src/Darvin/Fileman/Manager/AbstractManager.php
+++ src/Darvin/Fileman/Manager/AbstractManager.php
@@ -92,6 +92,6 @@ abstract class AbstractManager
             $suffix = '_'.preg_replace('/[^0-9a-z]+/i', '-', $suffix);
         }
 
-        return sprintf('%s%s_%s.zip', str_replace(DIRECTORY_SEPARATOR, '_', $dir), $suffix, $this->now->format('d-m-Y_H-i-s'));
+        return sprintf('%s%s_%s.zip', str_replace(DIRECTORY_SEPARATOR, '_', $dir), $suffix, $this->now->format('Y-m-d_H-i'));
     }
 }
